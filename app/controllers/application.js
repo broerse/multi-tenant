2a5bf00f491ffb4da1b5ab622cc1712315fb6f5e
Martin Broerse <broerse@martinic.com>
2016-10-23 12:38:28 +0200

    Move 'computed' to service

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,14 +1,9 @@
 import Ember from 'ember';
 import config from '../config/environment';
 
-const { get, computed, Controller, inject: {service} } = Ember;
+const { Controller, inject: {service} } = Ember;
 
 export default Controller.extend({
   getHost: service(),
-
-  hostname: computed(function() {
-    return get(this, 'getHost').hostname();
-  }),
-
   tenant: config.APP.tenant
 });
--- app/services/get-host.js
+++ app/services/get-host.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.Service.extend({
-  hostname: function() {
+const { computed, Service } = Ember;
+
+export default Service.extend({
+  hostname: computed(function() {
     if(window && window.location) {
       return window.location.host;
     }
-  }
+  })
 });
